70d2db4691b00ca1db390ad3fda0ae8d78d66184
Ariel <ariel@craftech.io>
2020-06-27 18:42:34 -0300

    New folder structure

--- extras/scripts/build.sh
+++ extras/scripts/build.sh
@@ -4,9 +4,11 @@ echo "# Executing latex build..."
 
 python extras/scripts/compile_refs.py main > /dev/null
 pdflatex -8bit main.tex > /dev/null
-mv main.tex tmp.tex
+makeindex main.nlo -s nomencl.ist -o main.els
+pdflatex -8bit main.tex > /dev/null
+# mv main.tex tmp.tex
 mv main.pdf PI.pdf
-rm main.*
-mv tmp.tex main.tex
+# rm main.*
+# mv tmp.tex main.tex
 
 echo "# Done. PDF file saved to .aux/PI.pdf"
